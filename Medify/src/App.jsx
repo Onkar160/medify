a4a35729c5ea4d6e233800a5d463606e09058197
Onkar <ophulwalkar77@gmail.com>
2025-06-15 22:19:36 +0530

    feat(ui): create MUI theme, add responsive navbar

--- Medify/src/App.jsx
+++ Medify/src/App.jsx
@@ -1,13 +1,12 @@
-
+import Home from "./Home/Home";
+import { Outlet } from "react-router";
 
 function App() {
-  
-
   return (
-    <div style={{height: "100vh", backgroundColor: "white"}}>
-      
+    <div style={{ height: "100vh", backgroundColor: "white" }}>
+      <Outlet />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- Medify/src/main.jsx
+++ Medify/src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./Home/Home.jsx";
+import Search from "./Search/Search.jsx";
+import MyBookings from "./MyBookings/MyBookings.jsx";
 
 const theme = createTheme({
   palette: {
@@ -13,12 +17,29 @@ const theme = createTheme({
       dark: "#0C8CE5",
     },
   },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: "none", // Disables uppercase
+        },
+      },
+    },
+  },
 });
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: App,
+    children: [{path: "/", Component: Home}, { path: "search", Component: Search }, {path: "my-bookings", Component: MyBookings}],
+  },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 );
