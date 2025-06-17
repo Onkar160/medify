36286e224e0f50e2a46c7d0043c47b3deea06322
Onkar <ophulwalkar77@gmail.com>
2025-06-17 17:28:42 +0530

    feat: add global state for form-data and search-data sharing

--- Medify/src/App.jsx
+++ Medify/src/App.jsx
@@ -1,10 +1,24 @@
 import Home from "./Home/Home";
+import { useState } from "react";
 import { Outlet } from "react-router";
 
 function App() {
+  const [selectedState, setSelectedState] = useState("");
+  const [selectedCity, setSelectedCity] = useState("");
+  const [hospitalsList, setHospitalsList] = useState([]);
+
   return (
     <div style={{ height: "100vh", backgroundColor: "white" }}>
-      <Outlet />
+      <Outlet
+        context={{
+          selectedState,
+          setSelectedState,
+          selectedCity,
+          setSelectedCity,
+          hospitalsList,
+          setHospitalsList,
+        }}
+      />
     </div>
   );
 }
