32995493133f6b9d84633d487fc831b1bc6dd184
Onkar <ophulwalkar77@gmail.com>
2025-06-18 16:18:21 +0530

    refactor: remove global state

--- Medify/src/App.jsx
+++ Medify/src/App.jsx
@@ -3,22 +3,9 @@ import { useState } from "react";
 import { Outlet } from "react-router";
 
 function App() {
-  const [selectedState, setSelectedState] = useState("");
-  const [selectedCity, setSelectedCity] = useState("");
-  const [hospitalsList, setHospitalsList] = useState([]);
-
   return (
     <div style={{ height: "100vh", backgroundColor: "white" }}>
-      <Outlet
-        context={{
-          selectedState,
-          setSelectedState,
-          selectedCity,
-          setSelectedCity,
-          hospitalsList,
-          setHospitalsList,
-        }}
-      />
+      <Outlet />
     </div>
   );
 }
