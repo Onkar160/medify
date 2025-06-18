b5d066f14c8ef810f0b4c1e291ddd66efa4ed5d1
Onkar <ophulwalkar77@gmail.com>
2025-06-18 22:26:14 +0530

    style: add height 100% to parent box

--- Medify/src/components/IconCard/IconCard.jsx
+++ Medify/src/components/IconCard/IconCard.jsx
@@ -9,6 +9,7 @@ export default function IconCard({
 }) {
   return (
     <Box
+      height="100%"
       borderRadius="10px"
       border={selected ? "2px solid #2AA7FF" : "none"}
       display="flex"
@@ -19,8 +20,18 @@ export default function IconCard({
       py={4}
       boxShadow={boxshadow ? "0px 34px 44px #D5DBE470 " : "none"}
     >
-      <img src={icon} alt={`${text}_icon`} style={{objectFit: "cover", width: "60px", height: "60px"}} />
-      <Typography marginTop="20px" variant="h5" color="#ABB6C7" px={3}>
+      <img
+        src={icon}
+        alt={`${text}_icon`}
+        style={{ objectFit: "cover", width: "60px", height: "60px" }}
+      />
+      <Typography
+        marginTop="20px"
+        variant="h5"
+        color="#ABB6C7"
+        px={3}
+        textAlign="center"
+      >
         {text}
       </Typography>
     </Box>
