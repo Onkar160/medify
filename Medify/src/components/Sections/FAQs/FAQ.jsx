40906051e059736a1f92eb3b8a85f59103af7352
Onkar <ophulwalkar77@gmail.com>
2025-06-21 11:43:34 +0530

    refactor: extract Accordion into seperate component and integrate into FAQ section

--- Medify/src/components/Sections/FAQs/FAQ.jsx
+++ Medify/src/components/Sections/FAQs/FAQ.jsx
@@ -1,18 +1,8 @@
-import {
-  Accordion,
-  AccordionSummary,
-  AccordionDetails,
-  Typography,
-  Box,
-  Container,
-  Stack,
-} from "@mui/material";
+import { Typography, Box, Container, Stack } from "@mui/material";
 import DoctorPatient from "../../../assets/doctor_patient_talk.jpg";
-import { IoMdAdd, IoMdClose } from "react-icons/io";
-import { useState } from "react";
+import MyAccordion from "../../Accordion/Accordion";
 
 export default function FAQs() {
-  const [expandedIndex, setExpandedIndex] = useState(null);
   const FAQ = [
     {
       question: "Why choose our medical for your family?",
@@ -82,64 +72,9 @@ export default function FAQs() {
               sx={{ justifyContent: "space-between" }}
               spacing={4}
             >
-              {FAQ.map((question, index) => {
-                const isOpen = expandedIndex === index;
-                return (
-                  <Accordion
-                    key={index}
-                    expanded={isOpen}
-                    onChange={() => setExpandedIndex(isOpen ? false : index)}
-                    sx={{
-                      boxShadow: "none",
-                      "&::before": {
-                        display: "none",
-                      },
-                    }}
-                    disableGutters
-                  >
-                    <AccordionSummary
-                      expandIcon={
-                        isOpen ? (
-                          <IoMdClose
-                            fontSize="23px"
-                            strokeWidth={25}
-                            color="#2AA7FF"
-                          />
-                        ) : (
-                          <IoMdAdd
-                            fontSize="23px"
-                            strokeWidth={25}
-                            color="#2AA7FF"
-                          />
-                        )
-                      }
-                      aria-controls="panel1-content"
-                      id="panel1-header"
-                      sx={{
-                        my: 0, // default margin
-                        "&.Mui-expanded": {
-                          minHeight: "48px", // default collapsed height
-                          my: 0, // remove margin on expand
-                        },
-                        "& .MuiAccordionSummary-content.Mui-expanded": {
-                          margin: 0, // remove spacing around content
-                        },
-                      }}
-                    >
-                      <Typography
-                        component="span"
-                        color="#1B3C74"
-                        fontWeight={600}
-                      >
-                        {question.question}
-                      </Typography>
-                    </AccordionSummary>
-                    <AccordionDetails sx={{ fontSize: "14px" }}>
-                      {question.answer}
-                    </AccordionDetails>
-                  </Accordion>
-                );
-              })}
+              {FAQ.map((question, index) => (
+                <MyAccordion questionData={question} index={index} />
+              ))}
             </Stack>
           </Box>
         </Box>
