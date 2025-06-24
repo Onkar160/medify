56e9a803cdcc1165a5cfaafe66be2a98cba120f4
Onkar <ophulwalkar77@gmail.com>
2025-06-24 18:19:11 +0530

    feat(ui): add BookingModal component section and add states

--- Medify/src/Search/Search.jsx
+++ Medify/src/Search/Search.jsx
@@ -3,19 +3,33 @@ import FAQs from "../components/Sections/FAQs/FAQ";
 import DownloadApp from "../components/Sections/DownloadApp/DownloadApp";
 import Footer from "../components/Footer/Footer";
 import SearchHospital from "../components/SearchHospital/SearchHospital";
-import { Box, Container, Typography, Stack } from "@mui/material";
-import { useState, useEffect, useMemo } from "react";
+import {
+  Box,
+  Container,
+  Typography,
+  Stack,
+  CircularProgress,
+} from "@mui/material";
+import { useState, useEffect, useMemo, useCallback } from "react";
 import { useSearchParams } from "react-router";
 import HospitalCard from "../components/HospitalCard/HospitalCard";
 import Banner from "../assets/appointment_banner.png";
 import { GoVerified } from "react-icons/go";
 import MyContext from "./MyContext";
 import BookingModal from "../components/BookingModal/BookingModal";
+import axios from "axios";
 
 export default function Search() {
   const [params, setParams] = useSearchParams();
   const [selectedState, setSelectedState] = useState("");
   const [selectedCity, setSelectedCity] = useState("");
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [hospitalList, setHospitalList] = useState([]);
+  // const [bookingEmail, setBookingEmail] = useState("");
+  const [bookingID, setBookingID] = useState(null);
+
   const timing = useMemo(
     () => [
       { period: "Morning", timings: ["11:30 AM"] },
@@ -31,18 +45,67 @@ export default function Search() {
     []
   );
 
+  const fetchHospitalsList = async () => {
+    try {
+      const response = await axios.get(
+        `https://meddata-backend.onrender.com/data?state=${selectedState}&city=${selectedCity}`
+      );
+      console.log(response.data);
+      setHospitalList(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const persistBookings = useCallback(
+    (bookingID, hospitalList, email, selectedDate, selectedTime) => {
+      let currentBookings = JSON.parse(localStorage.getItem("bookings")) || [];
+      let hospital = hospitalList.find(
+        (item) => item["Provider ID"] == bookingID
+      );
+      hospital.bookingEmail = email;
+      hospital.bookingDate = selectedDate;
+      hospital.bookingTime = selectedTime;
+      currentBookings.push(hospital);
+      localStorage.setItem("bookings", JSON.stringify(currentBookings));
+    }
+  );
+
   useEffect(() => {
     // console.log(params.get("city"), params.get("state"));
     const state = params.get("state");
     const city = params.get("city");
     if (state && city) {
+      setHospitalList([]);
       setSelectedState(state);
       setSelectedCity(city);
     }
   }, [params]);
 
+  useEffect(() => {
+    if (selectedState && selectedCity) {
+      fetchHospitalsList();
+    }
+  }, [selectedCity]);
+
+  useEffect(() => {
+    console.log(bookingID);
+  }, [bookingID]);
+
   return (
-    <MyContext.Provider value={{ timing }}>
+    <MyContext.Provider
+      value={{
+        timing,
+        selectedDate,
+        setSelectedDate,
+        selectedTime,
+        setSelectedTime,
+        modalOpen,
+        setModalOpen,
+        bookingID,
+        setBookingID,
+      }}
+    >
       <div>
         <NavBar background="white" />
         <Box
@@ -107,23 +170,49 @@ export default function Search() {
               display: "flex",
               flexDirection: { xs: "column", md: "row" },
               gap: 5,
+              flexWrap: "wrap",
+              justifyContent: { md: "center" },
             }}
           >
-            {/* <Box
-            width={{ sm: "100%", md: "70%" }}
-            backgroundColor="white"
-            height="fit-content"
-            p={3}
-            borderRadius={6}
-          >
-            <Typography variant="h4" fontWeight={500}>
-              Select state and city
-            </Typography>
-          </Box> */}
-            <Box width={{ sm: "100%", md: "70%" }}>
-              <HospitalCard type="Hospital Card" />
-            </Box>
-            <Box width={{ sm: "100%", md: "30%" }}>
+            <Stack width={{ sm: "100%", md: "100%", xl: "66%" }} spacing={3}>
+              {selectedCity && selectedState ? (
+                !hospitalList.length ? (
+                  <Box
+                    backgroundColor="white"
+                    height="fit-content"
+                    p={3}
+                    borderRadius={6}
+                    display="flex"
+                    justifyContent="center"
+                  >
+                    <CircularProgress />
+                  </Box>
+                ) : (
+                  hospitalList.map((hospital) => {
+                    return (
+                      <HospitalCard
+                        type="Hospital Card"
+                        hospital={hospital}
+                        key={hospital["Phone Number"]}
+                      />
+                    );
+                  })
+                )
+              ) : (
+                <Box
+                  backgroundColor="white"
+                  height="fit-content"
+                  p={3}
+                  borderRadius={6}
+                >
+                  <Typography variant="h4" fontWeight={500}>
+                    Select state and city
+                  </Typography>
+                </Box>
+              )}
+            </Stack>
+
+            <Box width={{ sm: "100%", xl: "30%" }}>
               <Box component="img" src={Banner} alt="Banner" width="100%" />
             </Box>
           </Box>
@@ -132,7 +221,15 @@ export default function Search() {
         <FAQs />
         <DownloadApp />
         <Footer />
-        <BookingModal />
+        <BookingModal
+          open={modalOpen}
+          setOpen={setModalOpen}
+          selectedDate={selectedDate}
+          selectedTime={selectedTime}
+          hospitalList={hospitalList}
+          bookingID={bookingID}
+          persistBookings={persistBookings}
+        />
       </div>
     </MyContext.Provider>
   );
